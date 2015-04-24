be8aed4e96666fd16947b74018d5d744d4b5d415
Omega86 <a911024@hotmail.hu>
2015-04-24 20:09:51 +0200

    A felesleges fájlokat töröltem. Remélem jó lesz.

--- test/TestDatabaseConnection.php
+++
@@ -1,5 +0,0 @@
-<?
-include("db_config.php");
-include("database.php");
-$db=new DatabaseConnection(DBConfig::$user,DBConfig::$password,DBConfig::$location,DBConfig::$dbname,DBConfig::$charset);
-echo $db->connect().PHP_EOL;
