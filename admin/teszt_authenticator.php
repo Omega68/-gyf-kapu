c555dbf4482007174e9135ef5e1fbd827740992f
Omega86 <a911024@hotmail.hu>
2015-05-01 16:54:35 +0200

    A ki és bejelentkezéshez szükséges komponensek legyártva. Admin felületen lehetséges a bejelentkezés.
    A következő issue-hoz tartozik: #18

--- admin/teszt_authenticator.php
+++ admin/teszt_authenticator.php
@@ -6,7 +6,9 @@ class Teszt_Authenticator extends Site_Authenticator{
   function isUserAuthorized(){
     //itt kell eldönteni, hogy be lehet-e engedi a user-t az adott felületre, ez esetben az adminra
     //Ha ezt átírod false-ra, akkor a login site-ot fogja mutatni.
-    return true;
+    $auth=Authentication::getInstance();
+      var_dump($auth->isLogged());
+    return $auth->isLogged();
   }
 }   
   
--- router/sites/admin_login_site.php
+++ router/sites/admin_login_site.php
@@ -3,16 +3,23 @@ return array(
   'default_menu'=>array()
   ,'template_class'=>"Admin_Login_Site_Template"
   ,'component_slots'=>array(
-    'login'=>""
+    'login'=>'authentikacio'
     ,'messages'=>'uzenetek'
   )
   ,'components'=>array(
     'uzenetek'=>array(
       'class'=>'Uzenetek_Site_Component'
       ,'allowed_slots'=>array()
+    ),
+        'login'=>array(
+            'class'=>'Admin_Login_Site_Component',
+            'params'=>array(),
+            'allowed_slots'=>array('login')
+        ),
+        'authentikacio'=>array(
+            'class'=>'Authentication_Site_Component',
+            'allowed_slots'=>array()
+        )
     )
-
-    
-  )
  
 );
