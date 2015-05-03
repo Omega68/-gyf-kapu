90f27a2dbfddb80cc8ba7cbac473f5017b3ed1cc
kruppakinga <kruppa.kinga@gmail.com>
2015-05-03 13:31:38 +0200

    Lapozás: találatok száma + refactor

--- modules/ugyfelek/ugyfelek_site_component.php
+++ modules/ugyfelek/ugyfelek_site_component.php
@@ -19,23 +19,74 @@ class Ugyfelek_Site_Component extends Site_Component{
     }
 
     function process(){
-        echo $_POST['selected'].' '.$_POST['next'].' '.$_POST['previous'];
         if(isset($_POST['deleteButton']) && isset($_POST['deleteAzon'])){
             $azon = $_POST['deleteAzon'];
             $u = $this->perm->getObjectsByField("Ugyfel", array('azon'=>$azon))[0];
             $u->delete();
         }
+        $this->pagination();
+
+    }
+
+    function show(){
+        $ugyfelek=$this->perm->getObjectsByLimitOffsetOrderBy("Ugyfel",$this->limit,$this->offset,'azon');
+        $osszes=$this->perm->getAllObjects("Ugyfel");
+        ?>
+
+            <div class="form_box">
+                <h1>Ugyfelek adatai</h1>
+            </div>
+            <br/>
+            <br/>
+        <?
+            $this->showPagination(count($osszes));
+        ?>
+
+            <div class="listtable">
+                <table style="width:100%">
+                        <tr>
+                            <th>#</th>
+                            <th>Azonosító</th>
+                            <th>Cim</th>
+                            <th>E-mail</th>
+                            <th>Telefon</th>
+                            <th>Törlés</th>
+                        </tr>
+
+                    <?
+                    $this->sorszam=$this->offset;
+                            foreach($ugyfelek as $f){
+                                echo '<tr>';
+                                    echo '<td>'.$this->sorszam.'</td>';
+                                    echo '<td>'.$f->getUgyfelFields()['azon'].'</td>';
+                                    echo '<td>'.$f->getUgyfelFields()['cim'].'</td>';
+                                    echo '<td>'.$f->getUgyfelFields()['email'].'</td>';
+                                    echo '<td>'.$f->getUgyfelFields()['telefon'].'</td>';
+                                    ?><td> <form action="" method="post">
+                                                    <input type="submit" name="deleteButton" value="Törlés" onclick="return confirm('Biztosan törli a kiválasztott ügyfelet?')" >
+                                                    <input type="hidden" name="deleteAzon" value="<? echo $f->getUgyfelFields()['azon'] ?>">
+                                                </form></td>
+                                    <?
+
+                                // echo '<td>'.$f->getUgyfelFields()['jelszo'].'</td>';
+                                echo '</tr>';
+                                $this->sorszam++;
+                            }
+                    ?>
+                </table>
+            </div>
+
+    <?
+
+        $this->showPagination(count($osszes));
+    }
+
+    private function pagination(){
+        //echo $_POST['selected'].' '.$_POST['next'].' '.$_POST['previous'];
+
         if(isset($_POST['selected']) && !isset($_POST['previous']) && !isset($_POST['next'])){
-            if($_POST['selected']==50){
-                $this->limit=50;
-                $this->offset=0;
-            }else if($_POST['selected']==100){
-                $this->limit=100;
-                $this->offset=0;
-            }else if($_POST['selected']==500){
-                $this->limit=500;
-                $this->offset=0;
-            }
+            $this->limit = $_POST['selected'];
+            $this->offset = 0;
 
         }
 
@@ -71,76 +122,37 @@ class Ugyfelek_Site_Component extends Site_Component{
             }
         }
         if(isset($_POST['selected']) && isset($_POST['next'])){
+            echo $this->offset . " " . $this->paginationNumber;
             if($_POST['selected']==50){
-                    $this->offset+=50;
-                    $this->paginationNumber++;
+                $this->offset+=50;
+                $this->paginationNumber++;
             }else if($_POST['selected']==100){
-                    $this->offset+=100;
-                    $this->paginationNumber++;
+                $this->offset+=100;
+                $this->paginationNumber++;
             }
             else if($_POST['selected']==500){
-                    $this->offset+=500;
-                    $this->paginationNumber++;
+                $this->offset+=500;
+                $this->paginationNumber++;
             }
         }
-
     }
 
-    function show(){
-        $ugyfelek=$this->perm->getObjectsByLimitOffsetOrderBy("Ugyfel",$this->limit,$this->offset,'azon');
+    private function showPagination($ugyfelek){
         ?>
-
-            <div class="form_box">
-                <h1>Ugyfelek adatai</h1>
-            </div>
-            <br/>
-            <br/>
-            <div class="listtable">
-                <table style="width:100%">
-                        <tr>
-                            <th>#</th>
-                            <th>Azonosító</th>
-                            <th>Cim</th>
-                            <th>E-mail</th>
-                            <th>Telefon</th>
-                            <th>Törlés</th>
-                        </tr>
-
-                    <?
-                    $this->sorszam=$this->offset;
-                            foreach($ugyfelek as $f){
-                                echo '<tr>';
-                                    echo '<td>'.$this->sorszam.'</td>';
-                                    echo '<td>'.$f->getUgyfelFields()['azon'].'</td>';
-                                    echo '<td>'.$f->getUgyfelFields()['cim'].'</td>';
-                                    echo '<td>'.$f->getUgyfelFields()['email'].'</td>';
-                                    echo '<td>'.$f->getUgyfelFields()['telefon'].'</td>';
-                                    ?><td> <form action="" method="post">
-                                                    <input type="submit" name="deleteButton" value="Törlés" onclick="return confirm('Biztosan törli a kiválasztott ügyfelet?')" >
-                                                    <input type="hidden" name="deleteAzon" value="<? echo $f->getUgyfelFields()['azon'] ?>">
-                                                </form></td>
-                                    <?
-
-                                // echo '<td>'.$f->getUgyfelFields()['jelszo'].'</td>';
-                                echo '</tr>';
-                                $this->sorszam++;
-                            }
-                    ?>
-                </table>
-            </div>
             <div class="pagination">
+                <p>Találatok száma: <? echo $ugyfelek;?></p>
                 <form action="" method="post">
-                 <select name="selected" onchange="this.form.submit()">
+                    <select name="selected" onchange="this.form.submit()">
                         <option value="50" <?if(empty($_POST['selected']) || $_POST['selected']==50) echo 'selected' ?> >50</option>
                         <option value="100" <?if($_POST['selected']==100) echo 'selected' ?>>100</option>
                         <option value="500" <?if($_POST['selected']==500) echo 'selected' ?>>500</option>
-                        </select> <input type="submit" name="previous" value="Előző">
-                 <span class="pagination_page_number">
-                        <span class="pagination_active_page_number"><?echo $this->paginationNumber;?></span>
-                </span>
+                    </select> <input type="submit" name="previous" value="Előző">
+                             <span class="pagination_page_number">
+                                    <span class="pagination_active_page_number"><?echo $this->paginationNumber;?></span>
+                            </span>
                     <input type="submit" name="next" value="Következő">
                 </form>
-                </div>
+            </div>
     <?
     }
 }
