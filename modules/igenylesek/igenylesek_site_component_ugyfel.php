ad9372b9af5e4affdf7c78e83040a57118617bd9
Omega86 <a911024@hotmail.hu>
2015-05-11 20:30:29 +0200

    A felülete az igénylés kitöltő oldalnak. #44
    Még nincs mögötte üzleti logika.

--- modules/igenylesek/igenylesek_site_component_ugyfel.php
+++ modules/igenylesek/igenylesek_site_component_ugyfel.php
@@ -13,6 +13,8 @@ class Igenylesek_Site_Component_Ugyfel extends Site_Component{
     private $paginationNumber=1;
     private $sorszam=1;
     private $szerkesztes=false;
+    private $uj_igenyles=false;
+    private $newSablonForm=false;
 
     protected function afterConstruction(){
         $this->perm=PersistenceManager::getInstance();
@@ -39,6 +41,19 @@ class Igenylesek_Site_Component_Ugyfel extends Site_Component{
 
         }
 
+        if(!empty($_POST['back']) || !empty($_POST['save'])){
+            $this->uj_igenyles=false;
+        }
+
+        if($_POST['UjIgenyles']){
+            $this->uj_igenyles=true;
+        }
+
+        if($_POST['Chosen'])
+        {
+            $this->newSablonForm=true;
+        }
+
         if(isset($_POST['editButton']) && isset($_POST['szerkAzon']))
             $this->szerkesztes=true;
         $this->pagination();
@@ -47,79 +62,12 @@ class Igenylesek_Site_Component_Ugyfel extends Site_Component{
 
     function show()
     {
-        if(!$this->szerkesztes) {
-            $uk=$this->perm->getObject($_SESSION['PHPSESSID']);
+        if($this->newSablonForm){
             $lekerdezes_adatok=array(
-                'ugyfel_azon'=>$uk->getFelhasznaloFields()['azon']
-            );
-            $igenylesek = $this->perm->getObjectsByFieldLimitOffsetOrderBy("Igenyles",$lekerdezes_adatok, $this->limit, $this->offset, 'azon');
-            //$igenylesek = $this->perm->getObjectsByLimitOffsetOrderBy("Igenyles", $this->limit, $this->offset, 'azon');
-            //    $igenylesek=$this->perm->getAllObjects("Igenyles");
-            echo '
-            <div class="form_box">
-                <h1>Igénylések adatai</h1>
-            </div>
-            <br/>
-            <br/>
-            <div class="listtable">
-                <table style="width:100%">
-                        <tr>
-                            <th>#</th>
-                            <th>Azonosító</th>
-                            <th>Státusz</th>
-                            <th>Létrehozás dátuma</th>
-                            <th>Utolsó módosítás dátuma</th>
-                            <th>Sablon azonosító</th>
-                            <th>Ügyfél azonosító</th>
-                            <th>Mező</th>
-                            <th>Szerkesztés</th>
-                            <th>Törlés</th>
-                        </tr>
-                        ';
-            $count = count($igenylesek);
-            $this->sorszam=$this->offset;
-            for ($i = 0; $i < $count; $i++) {
-                $iFields = $igenylesek[$i]->getIgenylesFields();
-                echo '<tr>';
-                echo '<td>'.($this->sorszam + 1.).'</td>';
-                echo '<td>' . $iFields['azon'] . '</td>';
-                echo '<td>' . $iFields['statusz'] . '</td>';
-                echo '<td>' . date("Y.m.d",strtotime($iFields['letrehozas_datuma'])) . '</td>';
-                echo '<td>' . date("Y.m.d",strtotime($iFields['utolso_modositas'])) . '</td>';
-                echo '<td>' . $iFields['sablon_azon'] . '</td>';
-                echo '<td>' . $iFields['ugyfel_azon'] . '</td>';
-
-                ?>
-
-                <td>
-
-                    <form action="" method="post">
-                        <input type="submit" name="editButton" value="Szerkesztés" >
-                        <input type="hidden" name="szerkAzon" value="<? echo $iFields['azon']?>">
-                    </form></td>
-
-
-                <td> <form action="" method="post">
-                        <input type="submit" name="deleteButton" value="Törlés" onclick="return confirm('Biztosan törli a kiválasztott igénylést?')" >
-                        <input type="hidden" name="deleteAzon" value="<? echo $iFields['azon'] ?>">
-                    </form></td>
-
-
-                <?
-                echo '</tr>';
-                $this->sorszam++;
-            }
-            echo '
-                </table>
-            </div>';
-            $this->showPagination(count($igenylesek));
-        }
-        else{
-            $lekerdezes_adatok=array(
-                'azon'=>"{$_POST['szerkAzon']}"
+                'sablon_azon'=>"{$_POST['sab_azon']}"
             );
             //var_dump($lekerdezes_adatok);
-            $customer=$this->perm->getObjectsByField('Igenyles',$lekerdezes_adatok);
+            $mezok=$this->perm->getObjectsByField('Mezo',$lekerdezes_adatok);
             // var_dump($customer);
             ?>
             <form action="" method="POST">
@@ -135,7 +83,35 @@ class Igenylesek_Site_Component_Ugyfel extends Site_Component{
                         <tr>
                             <td valign="top">
                                 <table>
-                                    <tbody>
+                                    <tbody><?
+                                        $count=count($mezok);
+                                        for($i=0;$i<$count;$i++){
+                                            echo '<tr>';
+                                                echo '<td><span>'.$mezok[$i]->getMezoFields()['azon'].'</span></td>';
+                                                if($mezok[$i]->getMezoFields()['tipus']=='Szám'){
+                                                    ?><td><input type="number" name="<?echo 'ertek'.$i?>" value=""> </td>
+                                                       <input type="hidden" name="<?echo 'azon'.$i?>" value="<?$mezok[$i]->getMezoFields()['azon']?>"?> <?
+                                                }
+                                                else if($mezok[$i]->getMezoFields()['tipus']=='Szöveg'){
+                                                ?><td><input type="text" name="<?echo 'ertek'.$i?>" value=""> </td>
+                                                <input type="hidden" name="<?echo 'azon'.$i?>" value="<?$mezok[$i]->getMezoFields()['azon']?>"?> <?
+
+                                                } else if($mezok[$i]->getMezoFields()['tipus']=='Legördülős'){
+                                               ?><td><select name="<?echo 'azon'.$i?>"> <?
+                                                    $mezo_adatok=array(
+                                                        'mezo_azon'=>$mezok[$i]->getMezoFields()['azon']
+                                                    );
+                                                     $ertekek=$this->perm->getObjectsByField('Ertek',$mezo_adatok);
+                                                     $ertekekSzama=count($ertekek);
+                                                    for($j=0;$j<$ertekekSzama;$j++){
+                                                        echo '<option>'.$ertekek[$j]->getErtekFields()['ertek'].'</option>';
+                                                     }
+                                                    echo  '</select></td>';
+                                            }
+                                            echo '</tr>';
+                                        }
+
+                                    /*
                                     <tr>
                                         <td><span>Azonosító</span></td>
                                         <td><input type="text" name="azon" readonly="readonly"  value="<? echo $customer[0]->getIgenylesFields()['azon'] ?>"></td>
@@ -159,7 +135,7 @@ class Igenylesek_Site_Component_Ugyfel extends Site_Component{
                                     <tr>
                                         <td><span>Ügyfel azonosító</span></td>
                                         <td><input type="text" name="ugyfel_azon" value="<?echo $customer[0]->getIgenylesFields()['ugyfel_azon']?>"></td>
-                                    </tr>
+                                    </tr>*/?>
                                     </tbody>
                                 </table>
                             </td>
@@ -170,7 +146,177 @@ class Igenylesek_Site_Component_Ugyfel extends Site_Component{
             </div>
             </form><?
         }
-    }
+       else if($this->uj_igenyles)
+        {
+            $sablonok=$this->perm->getObjectsByLimitOffsetOrderBy("UrlapSablon",$this->limit,$this->offset,'azon');
+            $osszes=$this->perm->getAllObjects("UrlapSablon");
+            echo '<form action="" method="post">
+                        <input type="submit" value="Vissza" name="back">
+            </form>';
+            echo '<form method="post">
+            <div class="form_box">
+                <h1>Sablonok adatai</h1>
+            </div>
+            <br/>
+            <br/>
+            <div class="listtable">
+                <table style="width:100%">
+                        <tr>
+                            <th>#</th>
+                            <th>Azonosító</th>
+                            <th>Létrehozás dátuma</th>
+                            <th>Állapot</th>
+                            <th>Létrehozó admin</th>
+                            <th>Művelet</th>
+                        </tr>
+                        ';
+            $this->sorszam=$this->offset;
+            $count=count($sablonok);
+            for($i=0;$i<$count;$i++){
+                $s = $sablonok[$i]->getUrlapSablonFields();
+                echo '<tr>';
+                echo '<td>'.($this->sorszam + 1) . '</td>';
+                echo '<td>'.$s['azon'].'</td>';
+                echo '<td>'.date("Y.m.d",strtotime($s['letrehozas_datuma'])).'</td>';
+                echo '<td>'.$s['allapot'].'</td>';
+                echo '<td>'.$s['admin_azon'].'</td>';
+                echo '<form method="post">';
+                echo '<input type="hidden" name="sab_azon" value="'.$s['azon'].'">';
+                echo '<td> <input type="submit" name="Chosen" value="Kiválasztás"></td>';
+                echo '</form>';
+                echo '</tr>';
+                $this->sorszam++;
+            }
+            $this->showPagination(count($osszes));
+        }
+            //Listázás
+            else if(!$this->szerkesztes) {
+                $uk=$this->perm->getObject($_SESSION['PHPSESSID']);
+                $lekerdezes_adatok=array(
+                    'ugyfel_azon'=>$uk->getFelhasznaloFields()['azon']
+                );
+                $igenylesek = $this->perm->getObjectsByFieldLimitOffsetOrderBy("Igenyles",$lekerdezes_adatok, $this->limit, $this->offset, 'azon');
+                //$igenylesek = $this->perm->getObjectsByLimitOffsetOrderBy("Igenyles", $this->limit, $this->offset, 'azon');
+                //    $igenylesek=$this->perm->getAllObjects("Igenyles");
+                echo '
+            <div class="form_box">
+                <h1>Igénylések adatai</h1>
+                <form method="post">
+                    <input type="submit" name="UjIgenyles" value="Új igénylés hozzáadása" class="save_button">
+                </form>
+            </div>
+            <br/>
+            <br/>
+            <div class="listtable">
+                <table style="width:100%">
+                        <tr>
+                            <th>#</th>
+                            <th>Azonosító</th>
+                            <th>Státusz</th>
+                            <th>Létrehozás dátuma</th>
+                            <th>Utolsó módosítás dátuma</th>
+                            <th>Sablon azonosító</th>
+                            <th>Ügyfél azonosító</th>
+                            <th>Mező</th>
+                            <th>Szerkesztés</th>
+                            <th>Törlés</th>
+                        </tr>
+                        ';
+                $count = count($igenylesek);
+                $this->sorszam=$this->offset;
+                for ($i = 0; $i < $count; $i++) {
+                    $iFields = $igenylesek[$i]->getIgenylesFields();
+                    echo '<tr>';
+                    echo '<td>'.($this->sorszam + 1.).'</td>';
+                    echo '<td>' . $iFields['azon'] . '</td>';
+                    echo '<td>' . $iFields['statusz'] . '</td>';
+                    echo '<td>' . date("Y.m.d",strtotime($iFields['letrehozas_datuma'])) . '</td>';
+                    echo '<td>' . date("Y.m.d",strtotime($iFields['utolso_modositas'])) . '</td>';
+                    echo '<td>' . $iFields['sablon_azon'] . '</td>';
+                    echo '<td>' . $iFields['ugyfel_azon'] . '</td>';
+
+                    ?>
+
+                    <td>
+
+                        <form action="" method="post">
+                            <input type="submit" name="editButton" value="Szerkesztés" >
+                            <input type="hidden" name="szerkAzon" value="<? echo $iFields['azon']?>">
+                        </form></td>
+
+
+                    <td> <form action="" method="post">
+                            <input type="submit" name="deleteButton" value="Törlés" onclick="return confirm('Biztosan törli a kiválasztott igénylést?')" >
+                            <input type="hidden" name="deleteAzon" value="<? echo $iFields['azon'] ?>">
+                        </form></td>
+
+
+                    <?
+                    echo '</tr>';
+                    $this->sorszam++;
+                }
+                echo '
+                </table>
+            </div>';
+                $this->showPagination(count($igenylesek));
+            }
+            else{
+                $lekerdezes_adatok=array(
+                    'azon'=>"{$_POST['szerkAzon']}"
+                );
+                //var_dump($lekerdezes_adatok);
+                $customer=$this->perm->getObjectsByField('Igenyles',$lekerdezes_adatok);
+                // var_dump($customer);
+                ?>
+                <form action="" method="POST">
+                <div class="form_box">
+                    <h1>Igénylés adatainak módosítása</h1>
+                    <input type="submit" name="save" value="Mentés" class="save_button">
+                    <input type="submit" name="back" value="Vissza" class="back_button">
+                    <br/>
+                    <br/>
+                    <div>
+                        <table class="formtable">
+                            <tbody>
+                            <tr>
+                                <td valign="top">
+                                    <table>
+                                        <tbody>
+                                        <tr>
+                                            <td><span>Azonosító</span></td>
+                                            <td><input type="text" name="azon" readonly="readonly"  value="<? echo $customer[0]->getIgenylesFields()['azon'] ?>"></td>
+                                        </tr>
+                                        <tr>
+                                            <td><span>Státusz</span></td>
+                                            <td><input type="text" name="statusz" value="<?echo $customer[0]->getIgenylesFields()['statusz']?>"></td>
+                                        </tr>
+                                        <tr>
+                                            <td><span>Létrehozás dátuma</span></td>
+                                            <td><input type="date" name="letrehozas_datuma" value="<?echo $customer[0]->getIgenylesFields()['letrehozas_datuma']?>"></td>
+                                        </tr>
+                                        <tr>
+                                            <td><span>Utolsó dátuma</span></td>
+                                            <td><input type="date" name="utolso_modositas" value="<?echo $customer[0]->getIgenylesFields()['utolso_modositas']?>"></td>
+                                        </tr>
+                                        <tr>
+                                            <td><span>Sablon azonosító</span></td>
+                                            <td><input type="text" name="sablon_azon" value="<?echo $customer[0]->getIgenylesFields()['sablon_azon']?>"></td>
+                                        </tr>
+                                        <tr>
+                                            <td><span>Ügyfel azonosító</span></td>
+                                            <td><input type="text" name="ugyfel_azon" value="<?echo $customer[0]->getIgenylesFields()['ugyfel_azon']?>"></td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                </form><?
+            }
+        }
 
     private function pagination(){
         $this->limit=(isset($_POST['limit']) && !empty($_POST['limit'])) ? $_POST['limit'] : 50;
